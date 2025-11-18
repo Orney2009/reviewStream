35033d97eba3f859ac1b4bf1116a8db21a6608ce
Mr.G <godonou.daryl@gmail.com>
2025-11-18 16:10:37 +0100

    [FEAT]: postgresql connection configuration and tables models

---
+++ data/__init__.py
@@ -0,0 +1,5 @@
+"""
+this package give access to all data used in the project (like the database connection instance)
+"""
+
+from .db import db
---
+++ lib/__init__.py
@@ -0,0 +1,5 @@
+"""
+This package exports all necessary variables and functions used during the development of this project
+"""
+
+from .variables import imdb_file_path, pg_database, pg_dialect_and_driver, pg_host, pg_password, pg_port, pg_username
